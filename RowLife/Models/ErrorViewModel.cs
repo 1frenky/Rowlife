12423d53e78d371648e3630db0a2f433ccf1b275
1frenky <f_bondjames@hotmail.com>
2018-12-18 15:14:28 +0100

    Add project files.

---
+++ RowLife/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RowLife.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
