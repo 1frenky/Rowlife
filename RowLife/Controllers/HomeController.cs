227238cfea65802029e970ecc107578657020db8
1frenky <f_bondjames@hotmail.com>
2019-01-08 01:22:01 +0100

    Wat style aan de website gegeven

--- RowLife/Controllers/HomeController.cs
+++ RowLife/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace RowLife.Controllers
             return View();
         }
 
+        public IActionResult Voeding()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
